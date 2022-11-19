ddb696fd8521c89a6fd9b3b7bfa7f78e8d390a91
GERALD IZUCHUKWU <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2022-11-19 11:17:22 +0100

    Add files via upload

---
+++ metadata.js
@@ -0,0 +1,583 @@
+const nfts2 = [
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 1',
+		description: 'Reconnaisanse soldier',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		// reformedAttributes:{
+		//     'weapon':'Mobile Carbine Rifle',
+		//     'armor': true,
+		// },
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Mobile Carbine Rifle',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 68,
+			},
+			{
+				trait_type: 'Color',
+				value: 'green',
+			},
+			{
+				trait_type: 'Shape',
+				value: 'Rectangle',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 300,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 200,
+			},
+			{
+				trait_type: 'Sides',
+				value: 4,
+			},
+			{
+				trait_type: 'Radius',
+				value: 30,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 90,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 2',
+		description: 'Frontline Commander',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Intelligent Alien Ripper',
+			},
+			{
+				trait_type: 'Armor',
+				value: false,
+			},
+			{
+				trait_type: 'Strength',
+				value: 72,
+			},
+			{
+				trait_type: 'Color',
+				value: 'purple',
+			},
+			{
+				trait_type: 'Shape',
+				value: 'Circle',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 200,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 300,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 3',
+		description: 'Raiden Hat',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Fists',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 90,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'Square',
+			},
+			{
+				trait_type: 'Color',
+				value: 'blue',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 250,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 350,
+			},
+			{
+				trait_type: 'Sides',
+				value: 4,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 4',
+		description: 'Lieutenant, leader',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Futuristic Blaster',
+			},
+			{
+				trait_type: 'Armor',
+				value: false,
+			},
+			{
+				trait_type: 'Strength',
+				value: 83,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'polygon',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 150,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 300,
+			},
+			{
+				trait_type: 'Sides',
+				value: 6,
+			},
+			{
+				trait_type: 'Radius',
+				value: 12,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 25,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 5',
+		description: 'Reconnaisanse soldier',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Electropulsing sword',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 68,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'polygon',
+			},
+			{
+				trait_type: 'Color',
+				value: 'Red',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 200,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 450,
+			},
+			{
+				trait_type: 'Sides',
+				value: 7,
+			},
+			{
+				trait_type: 'Radius',
+				value: 22,
+			},
+			{
+				trait_type: 'Width',
+				value: 35,
+			},
+			{
+				trait_type: 'Height',
+				value: 25,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 6',
+		description: 'MechaMan, Damage relish',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'fists',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 60,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'triangle',
+			},
+			{
+				trait_type: 'Color',
+				value: 'Pink',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 180,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 90,
+			},
+			{
+				trait_type: 'Sides',
+				value: 3,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 7',
+		description: 'Captain of Squad 7, Dependable',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Electropulsing sword',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 83,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'Rectangle',
+			},
+			{
+				trait_type: 'Color',
+				value: 'Black',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 200,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 300,
+			},
+			{
+				trait_type: 'Sides',
+				value: 4,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 8',
+		description: 'Trench Soldier',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Shred digger',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 50,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'Circle',
+			},
+			{
+				trait_type: 'Color',
+				value: 'Red',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 200,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 300,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 9',
+		description:
+			'god, basically invincible, rank 1, leader of all 10 armies',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Super Zanpakuto',
+			},
+			{
+				trait_type: 'Armor',
+				value: false,
+			},
+			{
+				trait_type: 'Strength',
+				value: 100,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'oval',
+			},
+			{
+				trait_type: 'Color',
+				value: 'green',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 400,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 350,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+	{
+		format: 'CHIP-0007',
+		name: 'Fighter 10',
+		description: 'John Wicked',
+		collection: {
+			id: '1',
+			name: 'V-belt test',
+		},
+		attributes: [
+			{
+				trait_type: 'Weapon',
+				value: 'Rocket fists + dog',
+			},
+			{
+				trait_type: 'Armor',
+				value: true,
+			},
+			{
+				trait_type: 'Strength',
+				value: 90,
+			},
+			{
+				trait_type: 'Shape',
+				value: 'polygon',
+			},
+			{
+				trait_type: 'Color',
+				value: 'White',
+			},
+			{
+				trait_type: 'PositionX',
+				value: 200,
+			},
+			{
+				trait_type: 'PositionY',
+				value: 300,
+			},
+			{
+				trait_type: 'Sides',
+				value: 4,
+			},
+			{
+				trait_type: 'Radius',
+				value: 20,
+			},
+			{
+				trait_type: 'Width',
+				value: 30,
+			},
+			{
+				trait_type: 'Height',
+				value: 50,
+			},
+			{
+				trait_type: 'Slope',
+				value: 20,
+			},
+		],
+	},
+];
+
+export default nfts2;
---
+++ try2.js
@@ -0,0 +1,268 @@
+import nfts2 from './nfts.js';
+import nfts from './metadata.js';
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const Runner = Matter.Runner;
+const Bodies = Matter.Bodies;
+const Composite = Matter.Composite;
+
+//create an engine
+const engine = Engine.create();
+
+// create a renderer
+const render = Render.create({
+	element: document.body,
+	engine: engine,
+	options: {
+		// width: w,
+		// height: h,
+		wireframes: false,
+	},
+});
+
+const nftBodies = nfts.map((nft) => {
+	const { attributes } = nft;
+	const attributeHashMap = {}; //creates an object
+	attributes.forEach((attr) => {
+		// console.log(attr);
+		attributeHashMap[attr.trait_type.toLowerCase()] = attr.value;
+		// assigning  the attribute trait type to the attribute value
+	});
+
+	if (attributeHashMap.shape.toLowerCase() === 'rectangle') {
+		//since in reformed attribute shape is the key and rectangle is the value we are checking if
+		//the value of shape to lower case === rectangle
+		const rectangleObj = Bodies.rectangle(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.width,
+			attributeHashMap.height,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return rectangleObj;
+	}
+
+	if (attributeHashMap.shape.toLowerCase() === 'polygon') {
+		const triangleObj = Bodies.polygon(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.sides,
+			attributeHashMap.radius,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return triangleObj;
+	}
+
+	if (attributeHashMap.shape.toLowerCase() === 'circle') {
+		const circleObj = Bodies.circle(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.radius,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return circleObj;
+	}
+
+	if (attributeHashMap.shape.toLowerCase() === 'oval') {
+		const circleObj = Bodies.circle(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.radius,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return circleObj;
+	}
+
+	if (attributeHashMap.shape.toLowerCase() === 'square') {
+		const squareObj = Bodies.polygon(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.sides,
+			attributeHashMap.radius,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return squareObj;
+	}
+
+	if (attributeHashMap.shape.toLowerCase() === 'triangle') {
+		const triangleObj = Bodies.polygon(
+			attributeHashMap.positionx,
+			attributeHashMap.positiony,
+			attributeHashMap.sides,
+			attributeHashMap.radius,
+			{
+				render: {
+					fillStyle: attributeHashMap.color,
+					strokeStyle: 'grey',
+					lineWidth: 3,
+				},
+			}
+		);
+		return triangleObj;
+	}
+});
+
+// const addNFTBodies = () => {
+// 	const nftBoxes2 = nfts2.map((nft) => {
+// 		const { attributes } = nft;
+// 		const attributeHashMap = {}; //creates an object
+// 		attributes.forEach((attr) => {
+// 			console.log(attr);
+// 			attributeHashMap[attr.trait_type.toLowerCase()] = attr.value;
+// 			// assigning  the attribute trait type to the attribute value
+// 		});
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'rectangle') {
+// 			//since in reformed attribute shape is the key and rectangle is the value we are checking if
+// 			//the value of shape to lower case === rectangle
+// 			const rectangleObj = Bodies.rectangle(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.width,
+// 				attributeHashMap.height,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return rectangleObj;
+// 		}
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'polygon') {
+// 			const triangleObj = Bodies.polygon(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.sides,
+// 				attributeHashMap.radius,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return triangleObj;
+// 		}
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'circle') {
+// 			const circleObj = Bodies.circle(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.radius,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return circleObj;
+// 		}
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'oval') {
+// 			const circleObj = Bodies.circle(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.radius,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return circleObj;
+// 		}
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'square') {
+// 			const squareObj = Bodies.polygon(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.sides,
+// 				attributeHashMap.radius,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return squareObj;
+// 		}
+
+// 		if (attributeHashMap.shape.toLowerCase() === 'triangle') {
+// 			const triangleObj = Bodies.polygon(
+// 				attributeHashMap.positionx,
+// 				attributeHashMap.positiony,
+// 				attributeHashMap.sides,
+// 				attributeHashMap.radius,
+// 				{
+// 					render: {
+// 						fillStyle: attributeHashMap.color,
+// 						strokeStyle: 'grey',
+// 						lineWidth: 3,
+// 					},
+// 				}
+// 			);
+// 			return triangleObj;
+// 		}
+// 	});
+// 	// console.log(nftBoxes2);
+// 	console.log(triangleObj);
+// };
+
+// creating ground
+const ground = Bodies.rectangle(400, 610, 810, 60, {
+	// isSensor: true,
+	// isSleeping: true,
+	isStatic: true,
+	render: {
+		fillStyle: 'red',
+	},
+});
+
+// add all the bodies to the world
+Composite.add(engine.world, [...nftBodies, ground]);
+
+Render.run(render);
+
+const runner = Runner.create();
+
+Runner.run(runner, engine);
